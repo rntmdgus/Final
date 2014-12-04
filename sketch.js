1a8a690d8183b712da01b9451fc996caba6e203b
rntmdgus <rntmdgus@naver.com>
2014-12-04 16:26:52 +0900

    d
    d

--- sketch.js
+++ sketch.js
@@ -1,17 +1,15 @@
-var bg;
+var bg, wheel;
 
 function setup(){
    bg = loadImage("bg.png");
+   wheel = loadImage("wheel.png");
    createCanvas(1000,1000);
-
-
 }
 
 
 function draw(){
    background(bg);  
+   Image(wheel,0,0,1000,1000)
 
-   noStroke();
    noCursor();
- 
 }
