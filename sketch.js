4a70a050ef26b3b26c09ade8a32cd0eb9dcb909d
rntmdgus <rntmdgus@naver.com>
2014-12-09 16:53:16 +0900

    dwdwd
    wdw

--- sketch.js
+++ sketch.js
@@ -47,10 +47,10 @@ function draw(){
    //성
    if(countcastel < 50){
    //성1
-   image(castel1,0,100,300);
+   image(castel1,100,300,200,200);
    } else if(countcastel < 100){
    //성2
-   image(castel2,0,0,100,300); 
+   image(castel2,100,300,200,200); 
    } else{
    countcastel=0;
    }
