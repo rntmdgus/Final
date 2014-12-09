f95e121fed888554caded6e89565f85a0d65ace5
rntmdgus <rntmdgus@naver.com>
2014-12-09 16:50:23 +0900

    323
    32

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-var bg, wheel1, wheel2, castel1, castel2, flag;
+var bg, wheel1, wheel2, castel1, castel2, flag, countcastel;
 
 function setup(){
    createCanvas(2000,1000);
@@ -9,6 +9,8 @@ function setup(){
    castel1 = loadImage("castel1.png");
    castel2 = loadImage("castel2.png"); 
    flag = loadImage("flag.png");
+
+   countcastel=0;
 }
 
 
@@ -26,7 +28,17 @@ function draw(){
    var t1 = (new Date()%15000)/15000;
    var t2 = (new Date()%6000)/6000;
 
-
+   //성
+   if(countcastel < 50){
+   //성1
+   image(castel1,0,100,300);
+   } else if(countcastel < 100){
+   //성2
+   image(castel2,0,0,100,300); 
+   } else{
+   countcastel=0;
+   }
+   countcastel++;
 
 
    //관람차
@@ -38,7 +50,7 @@ function draw(){
       image(wheel2,1000+sin(t1*2*PI+2*PI/4*i)*250-100,500+cos(t1*2*PI+2*PI/4*i)*250-100,200,200);
    }
 
-   
+
    //깃발위아래
    image(falg,100,300+cos(t2*2*PI)*10,100,100);
 
