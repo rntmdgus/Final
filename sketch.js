2805ed97f2e9cc5c6aead8f7352abc9012f15f51
rntmdgus <rntmdgus@naver.com>
2014-12-11 00:28:41 +0900

    dwd

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-var bg, bg2, wheel1, wheel2, castel1, castel2, castel3, flag, countcastel;
+var bg, bg2, wheel1, wheel2, castel1, castel2, castel3, castel4, flag, countcastel;
 
 function setup(){
    createCanvas(2000,1000);
@@ -10,6 +10,7 @@ function setup(){
    castel1 = loadImage("castel1.png");
    castel2 = loadImage("castel2.png"); 
    castel3 = loadImage("castel3.png"); 
+   castel4 = loadImage("castel4.png"); 
    flag = loadImage("flag.png");
 
 }
@@ -22,22 +23,28 @@ function draw(){
 
    var t1 = (new Date()%15000)/15000;
    var t2 = (new Date()%5000)/5000;
-   var t3 = (new Date()%3000)/3000;
+   var t3 = (new Date()%2000)/2000;
 
    //깃발위아래
    image(flag,120,300+cos(t2*2*PI)*10,100,100);
    
 
    //성
-   if(t3 <0.3){
+   if(t3 <0.25){
     image(castel1,20,330,300,500);
-   }else if(t3 <0.6){
+   }else if(t3 <0.5){
     image(castel2,20,330,300,500);
-   }else{
+   }else if(t3 <0.75){
     image(castel3,20,330,300,500);
-   }  
+   }else{
+    image(castel4,20,330,300,500);
+   } 
+
+
 
    image(bg2,0,0,2000,1000);
+
+
 
       //관람차
    for(var i = 0 ; i < 8 ; i++){
@@ -46,6 +53,8 @@ function draw(){
    for(var i = 0 ; i < 4 ; i++){
       image(wheel2,1000+sin(t1*2*PI+2*PI/4*i)*250-100,500+cos(t1*2*PI+2*PI/4*i)*250-100,200,200);
    }
+
+
 
 }
 
