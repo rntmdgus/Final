10976a1885df80548555f1699af2f72d0779fd5f
rntmdgus <rntmdgus@naver.com>
2014-12-11 03:30:10 +0900

    wdwd

--- sketch.js
+++ sketch.js
@@ -74,11 +74,11 @@ function draw(){
       image(wheel2,750+sin(t1*2*PI+2*PI/4*i)*250-100,500+cos(t1*2*PI+2*PI/4*i)*250-100,200,200);
    }
 
-   
+
    var t=(new Date()%20000)/20000;
 
    //사람
-   if(50*sin(t*2*PI+PI/6*i)>=0){
+   if(50*sin(t*2*PI+PI/6*i)<0){
       image(humen,1450+sin(t1*2*PI+2*PI/4*i)*250-50,870+cos(t1*2*PI+2*PI/4*i)*20-50,100,100);
    }
    if(50*sin(t*2*PI+PI/6*i)>=0){
